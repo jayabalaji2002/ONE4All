18c58afbc384a7c58f95766a173e3f4514148567
jayabalaji2002 <balaashwin01@gmail.com>
2024-01-10 12:02:06 +0530

    Exclude Men,women,kids products not render bcz of ShopContext that prop.children

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Footer.css";
 import footer_logo from "../Assets/logo_big.png";
 import instagram_icon from '../Assets/instagram_icon.png'
-import pintester_icon from '../Assets/pintester_icon.png'
+// import pintester_icon from '../Assets/pintester_icon.png'
 import whatsapp_icon from '../Assets/whatsapp_icon.png'
 import x_icon from '../Assets/x_logoo.png'
 
--- client/src/Pages/ShopCategory.jsx
+++ client/src/Pages/ShopCategory.jsx
@@ -8,10 +8,10 @@ const ShopCategory = (props) => {
   const {all_product} = useContext(ShopContext);
   return (
     <div className='shop-category'> 
-      <img src={props.banner} alt="" />
+      <img className='shopcategory-banner' src={props.banner} alt="" />
       <div className="shopcategory-indexSort">
         <p>
-          <span>Showing 1-12</span>out of 36 products
+          <span>Showing 1-12</span> out of 36 products
         </p>
         <div className="shopcategory-sort">
           Sort by <img src={dropdown_icon} alt="" />
@@ -31,6 +31,9 @@ const ShopCategory = (props) => {
               return null;
             }
         })}
+      </div>
+      <div className="shopcategory-loadmore">
+        <p>Explore More</p>
       </div>
     </div>
   )
