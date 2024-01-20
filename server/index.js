d3bbddc2c4fa46aa029199420c5d031f2ca85767
jayabalaji2002 <balaashwin01@gmail.com>
2024-01-20 10:53:30 +0530

    Get All products

--- server/index.js
+++ server/index.js
@@ -51,90 +51,92 @@ app.post("/upload", upload.single("product"), (req, res) => {
 
 // Schema for creating Products
 
-const ProductModel = mongoose.model("Product",{
-    id:{
-        type:Number,
-        required:true
-    },
-    name:{
-        type:String,
-        required:true
-    },
-    image:{
-        type:String,
-        required:true
-    },
-    category:{
-        type:String,
-        required:true
-    },
-    new_price:{
-        type:Number,
-        required:true
-    },
-    old_price:{
-        type:Number,
-        required:true
-    },
-    date:{
-        type:Date,
-        default:Date.now
-    },
-    available:{
-        type:Boolean,
-        default:true
-    }
-})
+const ProductModel = mongoose.model("Product", {
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  new_price: {
+    type: Number,
+    required: true,
+  },
+  old_price: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  available: {
+    type: Boolean,
+    default: true,
+  },
+});
 
-app.post('/addproduct',async(req,res)=>{
+app.post("/addproduct", async (req, res) => {
+  let products = await ProductModel.find({});
+  let id;
+  if (products.length > 0) {
+    let last_product_array = products.slice(-1);
+    let last_product = last_product_array[0];
+    id = last_product.id + 1;
+  } else {
+    id = 1;
+  }
 
-    let products = await ProductModel.find({});
-    let id;
-    if(products.length>0){
-        let last_product_array = products.slice(-1);
-        let last_product = last_product_array[0];
-        id = last_product.id+1;
+  const newProduct = new ProductModel({
+    id: id,
+    name: req.body.name,
+    image: req.body.image,
+    category: req.body.category,
+    new_price: req.body.new_price,
+    old_price: req.body.old_price,
+  });
 
-    }
-    else{
-        id = 1;
-    }
+  console.log(newProduct);
 
-    const newProduct = new ProductModel({
-        id:id,
-        name:req.body.name,   
-        image:req.body.image,
-        category:req.body.category,
-        new_price:req.body.new_price,
-        old_price:req.body.old_price
-    })
+  await newProduct.save();
 
-    console.log(newProduct);
+  console.log("Saved!");
 
-    await newProduct.save();
-
-    console.log("Saved!");
-
-    res.json({
-        success:true,
-        name:req.body.name
-    })
-
-})
-
+  res.json({
+    success: true,
+    name: req.body.name,
+  });
+});
 
 // Creating API for deleting product from DB
 
-app.post('/removeproduct', async (req,res)=>{
-    await ProductModel.findOneAndDelete({id:req.body.id});
-    console.log("Removed");
-    res.json({
-        success:true,
-        name:req.body.name
-    })
-})
+app.post("/removeproduct", async (req, res) => {
+  await ProductModel.findOneAndDelete({ id: req.body.id });
+  console.log("Removed");
+  res.json({
+    success: true,
+    name: req.body.name,
+  });
+});
 
+// Creating API for Display all products
 
+app.get("/allproducts", async (req, res) => {
+  let products = await ProductModel.find({});
+  console.log("All Products Fetched!");
+
+  res.send(products);
+});
 
 app.listen(port, (error) => {
   if (!error) {
