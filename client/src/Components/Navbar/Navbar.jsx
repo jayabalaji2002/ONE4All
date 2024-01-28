5e7264bdc20a15af903ebc22f31ed570325e7f2a
jayabalaji2002 <balaashwin01@gmail.com>
2024-01-28 21:41:33 +0530

    nav Adjustment

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,22 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import "../Navbar/Navbar.css";
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
 import { Link } from "react-router-dom";
 import { ShopContext } from "../../Context/ShopContext";
+import nav_dropdown from '../Assets/nav_dropdown.png'
+
 
 function Navbar() {
   // Moved the useState hook here to declare 'menu' state and 'setMenu' function.
   const [menu, setMenu] = useState("shop");
   const { getTotalCartItems } = useContext(ShopContext);
+
+  const menuRef  = useRef();
+  const dropdown_toggle = (e)=>{
+    menuRef.current.classList.toggle('nav-menu-visible')
+    e.target.classList.toggle('open')
+  }
 
   return (
     <div className="navbar">
@@ -18,7 +26,8 @@ function Navbar() {
         </Link>
         <p>ONE4ALL</p>
       </div>
-      <ul className="nav-menu">
+      <img src={nav_dropdown} alt="" onClick={dropdown_toggle} className="nav-dropdown"/>
+      <ul className="nav-menu" ref={menuRef}>
         {/* Utilizing 'menu' state and 'setMenu' function in event handlers */}
         <li
           onClick={() => {
